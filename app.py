acc772a9856147e79fe23e54dd2575e7e2dce714
VenkateshKrishnan <58022137+VenkateshKrishnan@users.noreply.github.com>
2022-02-10 19:03:40 +0800

    updated to image2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image2.jpg"), output_image_path=os.path.join(execution_path , "image2new.jpg"))
 
 for eachObject in detections:
         print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
